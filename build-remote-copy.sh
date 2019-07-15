869f1484f003ba057da66ef521d5228800d3c160
Artur Mich <audi2014@gmail.com>
2019-07-15 13:15:15 +0300

    build and send to remote by branch name

--- build-remote-copy.sh
+++ build-remote-copy.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
+#----------------------------------------------------------
+echo "validate git branch..."
+git_branch="$(git symbolic-ref --short -q HEAD)"
 
-react-scripts build
+case $git_branch in
+    development)
+        git_env="development"
+    ;;
+    sandbox)
+        git_env="sandbox"
+    ;;
+    production)
+        git_env="production"
+    ;;
+    *)
+        echo "ERROR: WRONG GIT BRANCH ($git_branch)"
+        exit 1;
+    ;;
+esac
 
+echo "ENV: $git_env"
+env-cmd -e $git_env react-scripts build
+
+
+#----------------------------------------------------------
 echo "copy to remote..."
-
 host="ubuntu@ec2-52-62-149-101.ap-southeast-2.compute.amazonaws.com"
 pem_path="/Users/arturmich/.ssh/loyaltyland-eb-ssh"
-current_env=$REACT_APP_ENVIRONMENT
-remote_path="/opt/bitnami/apps/wordpress/htdocs/dashboard-$current_env"
+remote_path="/opt/bitnami/apps/wordpress/htdocs/dashboard-$git_env"
 build_path="./build"
+#----------------------------------------------------------
 echo "remove remote build folder ($remote_path)..."
 ssh -i $pem_path ubuntu@ec2-52-62-149-101.ap-southeast-2.compute.amazonaws.com "ls $remote_path/ && rm -r $remote_path/"
 echo "create remote build folder ($remote_path)..."
